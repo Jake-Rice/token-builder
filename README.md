1a75fec1a16ccfac215bbc3daf245e98b3efa741
Jake Rice <jake@Lappy.local>
2022-03-11 08:53:42 -0800

    Added CustomERC20 contract

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This web app allows users to create their own custom ERC20 tokens.
 
 Once the token is created, the user has ownership of the new token contract.
 
-The owner of any ERC20 token contract can mint additional tokens.
+The owner of any custom ERC20 token contract can mint additional tokens.
 
-Holders of any ERC20 token can transfer the tokens or burn them.
+Holders of any ERC20 token can transfer the tokens, give an allowance to another address, or burn them.
+
+Addresses with an allowance can withdraw the tokens to their account, or send them to a third address.
--- contracts/CustomERC20.sol
+++ contracts/CustomERC20.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract CustomERC20 is ERC20, ERC20Detailed, ERC20Burnable {
-    address owner;
+contract CustomERC20 is ERC20, ERC20Burnable {
+    address public owner;
+    uint8 private dec;
     
-    constructor(address _tokenOwner, _name, _symbol, _decimals) {
+    constructor(address _tokenOwner, uint256 _initialSupply, string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
         owner = _tokenOwner; //passes ownership of token contract to address specified by TokenFactory
+        dec = _decimals;
+        _mint(owner, _initialSupply);
     }
 
     modifier OnlyOwner {
@@ -17,7 +19,17 @@ contract CustomERC20 is ERC20, ERC20Detailed, ERC20Burnable {
         _;
     }
 
-    function mint(uint amount) public OnlyOwner {
+    //allows owner to mint additional tokens to the owner address
+    function mint(uint256 amount) public OnlyOwner {
         _mint(owner, amount);
+    }
+
+    //allows owner to mint additional tokens to any non-zero address
+    function mintTo(address account, uint256 amount) public OnlyOwner {
+        _mint(owner, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return dec;
     }
 }
